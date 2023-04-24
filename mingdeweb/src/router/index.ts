e1e26ac80ad582d1d0f64d6906191cca872a84f7
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2023-04-24 13:42:20 +0800

    客户端按需加载element-plus前端组件

--- mingdeweb/src/router/index.ts
+++ mingdeweb/src/router/index.ts
@@ -1,8 +1,25 @@
-import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
 // 路由列表
 const routes: Array<RouteRecordRaw> = [
-
+  {
+    meta: {
+      title: "明德在线教育-首页",
+      keepAlive: true
+    },
+    path: '/',         // uri访问地址
+    name: "Home",
+    component: () => import("../views/Home.vue")
+  },
+  {
+    meta: {
+      title: "明德在线教育-用户登录",
+      keepAlive: true
+    },
+    path: '/login',      // uri访问地址
+    name: "Login",
+    component: () => import("../views/Login.vue")
+  }
 ]
 
 // 路由对象实例化
---
+++ mingdeweb/src/views/Login.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>登录</h1>
+</template>
+  
+<script setup lang="ts">
+
+</script>
+  
+<style scoped>
+</style>
